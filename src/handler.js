121f37dc2d237649ebd5cc1bca4f2b722601b354
fatkhu rohman <fatkhurohman019@gmail.com>
2023-07-31 23:20:25 +0700

    implement delete book by id

--- src/handler.js
+++ src/handler.js
@@ -181,9 +181,32 @@ const updateBookByIdHandler = (request, h) => {
   return response;
 };
 
+const deleteBookById = (request, h) => {
+  const { bookId } = request.params;
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    books.slice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   saveBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
   updateBookByIdHandler,
+  deleteBookById,
 };
