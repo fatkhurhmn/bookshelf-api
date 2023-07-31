3a16116c52c15948f80220d44b9a6b71554c0ecb
fatkhu rohman <fatkhurohman019@gmail.com>
2023-07-31 22:21:01 +0700

    implement get book by id

--- src/handler.js
+++ src/handler.js
@@ -91,4 +91,28 @@ const getAllBooksHandler = (_, h) => {
   return response;
 };
 
-module.exports = { saveBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const book = books.filter((b) => b.id === bookId)[0];
+  if (book !== undefined) {
+    const response = h.response({
+      status: 'success',
+      data: {
+        book,
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+
+  response.code(404);
+  return response;
+};
+
+module.exports = { saveBookHandler, getAllBooksHandler, getBookByIdHandler };
