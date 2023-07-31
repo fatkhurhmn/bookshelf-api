0aeaa030beacbcad24767c2bc06253a298e29425
fatkhu rohman <fatkhurohman019@gmail.com>
2023-07-31 22:52:03 +0700

    implement update book by id

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,9 @@
-const { saveBookHandler, getAllBooksHandler, getBookByIdHandler } = require('./handler');
+const {
+  saveBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  updateBookByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
@@ -15,6 +20,11 @@ const routes = [
     method: 'GET',
     path: '/books/{bookId}',
     handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: updateBookByIdHandler,
   },
 ];
 
